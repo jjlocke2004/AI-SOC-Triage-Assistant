1d37aaf4023132d4c3ef9819f32ae26f61a89c61
Jonathon Locke <jjlocke2004@gmail.com>
2026-04-09 14:05:29 -0400

    Attack Maping

---
+++ src/attack_mapping.py
@@ -0,0 +1,27 @@
+ATTACK_MAP = {
+    "encoded_powershell": {
+        "tactic": "Execution",
+        "technique": "Command and Scripting Interpreter: Powershell" ,
+        "attack_id": "T1059.001"
+    },
+    "brute_force": {
+        "tactic": "Credential Access",
+        "technique": "Brute Force",
+        "attack_id": "T1110"
+    },
+    "temp_execution": {
+        "tactic": "Execution",
+        "technique": "User Execution",
+        "attack_id": "T1204"
+    },
+    "powershell_network": {
+        "tactic": "Execution",
+        "technique": "Command and Scripting Interpreter: Powershell" ,
+        "attack_id": "T1059.001"
+    },
+    "benign": {
+        "tactic": "None",
+        "technique": "None",
+        "attack_id": "None"
+    }
+}
